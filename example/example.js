13617ba239ebd38a27470efe47470772022e8679
Jean Lauliac <lauliacj@gmail.com>
2014-01-14 18:19:39 +0100

    Convert the example JS to browserify

--- example/example.js
+++ example/example.js
@@ -1,136 +1,141 @@
-(function() {
+'use strict';
 
-    // Store the complete dataset of the chart.
-    //
-    var data = null;
+var d3 = require('d3');
+var chart = require('../src/chart');
+var colorbrewer = require('./colorbrewer');
+var dataSource = require('./data');
+var utils = require('./utils');
 
-    // Store the displayed dataset of the chart.
-    //
-    var filteredData = null;
+// Store the complete dataset of the chart.
+//
+var data = null;
 
-    // Current display options.
-    var options = {hourCount: 24, dayHours: [0,24]};
-    options.weekDays = document.getElementById('week-days').value.split(',');
+// Store the displayed dataset of the chart.
+//
+var filteredData = null;
 
-    // Initial color palette to use.
-    //
-    var INIT_PALETTE = 'RdBu';
+// Current display options.
+var options = {hourCount: 24, dayHours: [0, 24]};
+options.weekDays = document.getElementById('week-days').value.split(',');
 
-    // Create the color scale from the palette.
-    //
-    var colorScale = d3.scale.quantize().domain([0,1])
-                       .range(colorbrewer[INIT_PALETTE][9]);
+// Initial color palette to use.
+//
+var INIT_PALETTE = 'RdBu';
 
-    // Create the chart in an SVG element.
-    //
-    var chart = d3.select("#vis")
-                  .append("svg")
-                  .chart("BubbleMatrix");
+// Create the color scale from the palette.
+//
+var colorScale = d3.scale.quantize().domain([0, 1])
+                   .range(colorbrewer[INIT_PALETTE][9]);
 
-    // Set up the rows.
-    //
-    chart.rows(function (d) { return d.days; })
-         .rowKey(function (d) { return d.key; })
-         .rowHeader(function (d) { return dataSource.DayNames[d.key]; })
-         .rowData(function (d) { return d.data; })
+// Create the chart in an SVG element.
+//
+var chart = d3.select('#vis')
+              .append('svg')
+              .chart('BubbleMatrix');
 
-    // Set up the colums.
-    //
-    chart.columns(function (d) { return d3.range(d.dayHours[0],
-                                                 d.dayHours[1]); })
-         .colKey(function(d) { return d; })
-         .colHeader(utils.hourName)
+// Set up the rows.
+//
+chart.rows(function (d) { return d.days; })
+     .rowKey(function (d) { return d.key; })
+     .rowHeader(function (d) { return dataSource.DayNames[d.key]; })
+     .rowData(function (d) { return d.data; });
 
-    // Set up the bubbles.
-    //
-    chart.size(function (d) { return d.volume; })
-         .color(function (d) { return d.positivity; })
-         .colorScale(utils.nullableScale('#ddd', colorScale))
+// Set up the colums.
+//
+chart.columns(function (d) {
+    return d3.range(d.dayHours[0], d.dayHours[1]);
+})
+     .colKey(function (d) { return d; })
+     .colHeader(utils.hourName);
 
-    // The margin is the space allocated for row headers at the left. We can
-    // listen to its change and move the chart so that bubble threads are
-    // aligned with our paragraphs. `this` here refers to the chart, and
-    // `this.base` is a d3 selection on the SVG element, automatically
-    // provided by the chart.
-    //
-    chart.on('margin', function(value) {
-        this.base.style('margin-left', '-' + value + 'px');
+// Set up the bubbles.
+//
+chart.size(function (d) { return d.volume; })
+     .color(function (d) { return d.positivity; })
+     .colorScale(utils.nullableScale('#ddd', colorScale));
+
+// The margin is the space allocated for row headers at the left. We can
+// listen to its change and move the chart so that bubble threads are
+// aligned with our paragraphs. `this` here refers to the chart, and
+// `this.base` is a d3 selection on the SVG element, automatically
+// provided by the chart.
+//
+chart.on('margin', function(value) {
+    this.base.style('margin-left', '-' + value + 'px');
+});
+
+// We demonstrate here the ability to change some inner elements of the
+// chart easily. Here, we highlight the daytime hours by setting a
+// light color to other headers. Returning `null` makes the header
+// get the color defined by CSS.
+//
+chart.layer('col-header').on('enter', function () {
+    this.classed('night', function (d) {
+        return !(d > 6 && d < 20);
     });
+});
 
-    // We demonstrate here the ability to change some inner elements of the
-    // chart easily. Here, we highlight the daytime hours by setting a
-    // light color to other headers. Returning `null` makes the header
-    // get the color defined by CSS.
-    //
-    chart.layer('col-header').on('enter', function() {
-        this.classed('night', function(d) {
-            return !(d > 6 && d < 20);
-        });
-    });
+// Helpers.
+//
+function redraw() {
+    chart.draw(filteredData);
+}
 
-    // Helpers.
-    //
-    function redraw() {
-        chart.draw(filteredData);
-    }
+function refilter() {
+    filteredData = dataSource.filter(data, options);
+    redraw();
+}
 
-    function refilter() {
-        filteredData = dataSource.filter(data, options);
-        redraw();
-    }
+function rebuild() {
+    data = dataSource.generate();
+    refilter();
+}
 
-    function rebuild() {
-        data = dataSource.generate();
-        refilter();
-    }
+function resize() {
+    var width = window.innerWidth * 0.70;
+    chart.width(width);
+    chart.height(width * 0.3);
+}
 
-    function resize() {
-        width = window.innerWidth*0.70
-        chart.width(width);
-        chart.height(width*0.3);
-    }
-
-    function onResize() {
-        resize();
-        redraw();
-    }
-
-    window.addEventListener('resize', _.debounce(onResize, 100));
-    document.getElementById('refresh').addEventListener("click", rebuild);
-
-    document.getElementById('day-hours')
-        .addEventListener("change", function() {
-            options.dayHours = document.getElementById('day-hours').value.split(',');
-            options.dayHours[0] = +options.dayHours[0]
-            options.dayHours[1] = +options.dayHours[1]
-            refilter();
-    });
-
-    document.getElementById('week-days')
-        .addEventListener("change", function() {
-            options.weekDays = document.getElementById('week-days').value.split(',');
-            refilter();
-    });
-
-    var paletteSelect = document.getElementById('color-palette');
-
-    for (pl in colorbrewer) {
-        if (colorbrewer[pl][9] == undefined)
-            continue;
-        var el = document.createElement('option');
-        el.text = pl;
-        if (pl === INIT_PALETTE)
-            el.setAttribute('selected', 'selected');
-        paletteSelect.appendChild(el);
-    }
-
-    paletteSelect.addEventListener("change", function() {
-            var palette = paletteSelect.value;
-            colorScale.range(colorbrewer[palette][9]);
-            redraw();
-    });
-
+function onResize() {
     resize();
-    rebuild();
+    redraw();
+}
 
-}());
+window.addEventListener('resize', _.debounce(onResize, 100));
+document.getElementById('refresh').addEventListener('click', rebuild);
+
+document.getElementById('day-hours')
+    .addEventListener('change', function () {
+        options.dayHours = document.getElementById('day-hours').value.split(',');
+        options.dayHours[0] = +options.dayHours[0];
+        options.dayHours[1] = +options.dayHours[1];
+        refilter();
+    });
+
+document.getElementById('week-days')
+    .addEventListener("change", function() {
+        options.weekDays = document.getElementById('week-days').value.split(',');
+        refilter();
+});
+
+var paletteSelect = document.getElementById('color-palette');
+
+for (var pl in colorbrewer) {
+    if (typeof colorbrewer[pl][9] === 'undefined')
+        continue;
+    var el = document.createElement('option');
+    el.text = pl;
+    if (pl === INIT_PALETTE)
+        el.setAttribute('selected', 'selected');
+    paletteSelect.appendChild(el);
+}
+
+paletteSelect.addEventListener('change', function () {
+    var palette = paletteSelect.value;
+    colorScale.range(colorbrewer[palette][9]);
+    redraw();
+});
+
+resize();
+rebuild();
--- example/utils.js
+++ example/utils.js
@@ -1,40 +1,37 @@
-(function() {
-    utils = {}
+'use strict';
 
-    // Build a small hour name based on it's index from 0 to 24.
-    // Eg. '2p', meaning 2 P.M. Note that 12p is noon, 12a midnight.
-    //
-    utils.hourName = function(i) {
-        var letter = 'a';
-        if (i >= 12)
-            letter = 'p';
-        i = i % 12;
-        if (i === 0)
-            i = 12;
-        return i + letter;
-    }
+// Build a small hour name based on it's index from 0 to 24.
+// Eg. '2p', meaning 2 P.M. Note that 12p is noon, 12a midnight.
+//
+exports.hourName = function (i) {
+    var letter = 'a';
+    if (i >= 12)
+        letter = 'p';
+    i = i % 12;
+    if (i === 0)
+        i = 12;
+    return i + letter;
+};
 
-    // Create a 'nullable' scale, just like d3.js scales but allowing for
-    // `null` value to be passed as argument. `v` is the value to be returned
-    // when the value is `null`; call `fn` otherwise.
-    //
-    // You access the inner function/scale subsequently by using the `inner`
-    // function.
-    // Eg. `nullableScale(0, d3.scale.linear()).inner().domain([0,1])`
-    //
-    utils.nullableScale = function(v, fn) {
-        var innerFn = fn;
-        var result = function(c) {
-            if (c == null)
-                return v;
-            return innerFn(c);
-        }
-        result.inner = function(fn) {
-            if (fn == null)
-                return innerFn;
-            innerFn = fn;
-        }
-        return result;
-    }
-
-}());
+// Create a 'nullable' scale, just like d3.js scales but allowing for
+// `null` value to be passed as argument. `v` is the value to be returned
+// when the value is `null`; call `fn` otherwise.
+//
+// You access the inner function/scale subsequently by using the `inner`
+// function.
+// Eg. `nullableScale(0, d3.scale.linear()).inner().domain([0,1])`
+//
+exports.nullableScale = function (v, fn) {
+    var innerFn = fn;
+    var result = function (c) {
+        if (c === null || typeof c === 'undefined')
+            return v;
+        return innerFn(c);
+    };
+    result.inner = function (fn) {
+        if (typeof fn === 'undefined')
+            return innerFn;
+        innerFn = fn;
+    };
+    return result;
+};
